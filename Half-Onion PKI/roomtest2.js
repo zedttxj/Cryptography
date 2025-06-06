79ba6be727552ccaa6c06dd489799bda1d69529f
Nguyen Anh Minh <anhminh.nguyen@ace.tamut.edu>
2025-06-06 10:35:20 -0500

    Update roomtest2.js

--- Half-Onion PKI/roomtest2.js
+++ Half-Onion PKI/roomtest2.js
@@ -1,7 +1,11 @@
-const { runAuthenticatedClient } = require('./clientCertRequester');
+const { runAuthenticatedClient, runAuthenticatedClient2Ways, requestCertificateFromHub } = require('./clientCertRequester');
 
 (async () => {
-  const { certificate, privateKey } = await runAuthenticatedClient();
+  const { certificate, privateKey, caPublicKey, roomId } = await requestCertificateFromHub();
+  const { clientPubKey: publicKey} = await runAuthenticatedClient2Ways("ws://localhost:8888/hub", certificate, privateKey, caPublicKey, roomId);
   console.log(certificate);
   console.log(privateKey);
+  console.log(publicKey);
 })();
+
+// The same thing for roomtest.js
